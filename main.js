a8fe495b3e92f79c885fe8cbc6ea47c3b5df6b2a
ariptom <95114854+ariptom@users.noreply.github.com>
2023-01-14 16:23:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//mhs//
+function a() {
+    console.log("working");
+    document.getElementById("content").innerHTML="Mahbert High School has been affiliated by Council for the Indian School Certificate Examinations also known as CISCE. CISCE is globally accepted private board of school education which is popular for its balanced syllabus and its recognition by various foreign education systems in compare to any other board in India.CISCE board affilitaion code for Mahbert High School is WB107.English is endorsed language of communication in classrooms and Mahbert High School has authorized english as medium of instruction and preferred textbook language as well. Progressive management members are keen to provide competitive, practical and diverse environment to their students and hence they have opted to educate girls and boys in same classrooms. This school has been granted the affiliation for Indian Certificate of Secondary Education examination and Indian School Certificate examinations by Council for the Indian School Certificate Examinations that being said that this school is authorized to conduct examination for 12th and 10th both classes, which is good thing as student will not need switch school after 10th class.Mahbert High School does not provide boarding facility to there students.Mahbert High School is 1176th best school of India and its 170th best school out of 319 CISCE Schools in West Bengal.This school may come in category of average school but frankly its not average, statically speaking this school is slightly below average, if other better schools are available in area then its advisable to opt them how ever if for some reason one have to choose this school then its not hopeless, a lot of hard work and external guidance will be required to reach the full potential in exams.Out of ten we would say this school is four.A little more focus and effort of management can drastically improve the performance of this school.";
+}
+function b(){
+    console.log("workinng");
+    document.getElementById("content").innerHTML="";
+}
+//dbs//
+function a1(){
+    console.log("W");
+    document.getElementById("content").innerHTML="One of the best place for learning and developing knowledge and skills in Siliguri. Even during this challenging pandemic crisis, this schools management and faculties supported us..,the children and well as the parents for proper learning and guidance through online mode. We are very thankful to the Respected Father Manoj Jose and the entire team of DBS Siliguri for their dedication and untiring efforts. We appreciate and wish them to stay safe and take care of their family."
+}
+function b1(){
+    console.log("ww");
+    document.getElementById("content").innerHTML=""
+}
+//ac//
+function a2(){
+    console.log("W");
+    document.getElementById("content").innerHTML="Auxilium Convent School (ACS) located at Sevoke Road, Post Box No. 48 Jyotinagar Siliguri Jalpaiguri is one of the popular schools in India. The School has been rated by 16 people on iCBSE. The School has been recognized by Council for Indian School Certificate Examinations since a long time. The Auxilium Convent School has been viewed 5661 times by the visitors on iCBSE. This School is counted among the top-rated Schools in West Bengal with an excellent academic track record. If you're looking for more details regarding examinations schedule, admission procedure, syllabus, results and application forms, kindly contact the relevant department of the school."
+}
+function b2(){
+    console.log("ww");
+    document.getElementById("content").innerHTML=""
+}
+//nc//
+function a3(){
+    console.log("W");
+    document.getElementById("content").innerHTML="Nirmala Convent School (NCS) located at 3rd Mile, Sevoke Road P.o. Salugara Siliguri Jalpaiguri is one of the popular schools in India. The School has been rated by 4 people on iCBSE. It has been reviewed by 2 people with an average rating of 5 stars out of 5. The School has been recognized by Council for Indian School Certificate Examinations since a long time. The Nirmala Convent School has been viewed 4680 times by the visitors on iCBSE. This School is counted among the top-rated Schools in West Bengal with an excellent academic track record. If you're looking for more details regarding results, admission procedure, examinations schedule, syllabus and application forms, kindly contact the relevant department of the school."
+}
+function b3(){
+    console.log("ww");
+    document.getElementById("content").innerHTML=""
+}
